f231582e423c0ee85b950a6c1c230affc3a02aab
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-06-15 12:22:34 -0400

    rust-framebuffer: get simple framebuffer test working

--- rust-framebuffer/src/main.rs
+++ rust-framebuffer/src/main.rs
@@ -1,3 +1,25 @@
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = 360;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "Test window",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    ).unwrap();
+
+    window.set_target_fps(60);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for (i, pixel) in buffer.iter_mut().enumerate() {
+            *pixel = i as u32;
+        }
+
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
 }
