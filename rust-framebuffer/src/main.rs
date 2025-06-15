58af9f45941508833320f5080b955ba367b5ebc9
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-06-15 12:34:02 -0400

    rust-framebuffer: test out differing window/buffer sizes

--- rust-framebuffer/src/main.rs
+++ rust-framebuffer/src/main.rs
@@ -1,15 +1,19 @@
 use minifb::{Key, Window, WindowOptions};
 
-const WIDTH: usize = 640;
-const HEIGHT: usize = 360;
+const BUFFER_WIDTH: usize = 160;
+const BUFFER_HEIGHT: usize = 144;
+
+const WINDOW_SCALE: usize = 8;
+const WINDOW_WIDTH: usize = BUFFER_WIDTH * WINDOW_SCALE;
+const WINDOW_HEIGHT: usize = BUFFER_HEIGHT * WINDOW_SCALE;
 
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut buffer: Vec<u32> = vec![0; BUFFER_WIDTH * BUFFER_HEIGHT];
 
     let mut window = Window::new(
         "Test window",
-        WIDTH,
-        HEIGHT,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
         WindowOptions::default(),
     ).unwrap();
 
@@ -17,9 +21,12 @@ fn main() {
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         for (i, pixel) in buffer.iter_mut().enumerate() {
-            *pixel = i as u32;
+            let x = i % BUFFER_WIDTH;
+            let y = i / BUFFER_WIDTH;
+
+            *pixel = if (x + y) % 2 == 0 { 0xFFFFFFFF } else { 0x00000000 };
         }
 
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+        window.update_with_buffer(&buffer, BUFFER_WIDTH, BUFFER_HEIGHT).unwrap();
     }
 }
