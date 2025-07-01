f01809fafb09c5edb185c5bf8694aba13ad95be8
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-07-01 11:49:53 -0400

    vivado-pullup-test: add to repo

---
+++ vivado-pullup-test/constraints.xdc
@@ -0,0 +1,5 @@
+set_property -dict {PACKAGE_PIN E3 IOSTANDARD LVCMOS33} [get_ports clk]
+create_clock -period 10.000 -name sys_clk_pin -waveform {0.000 5.000} -add [get_ports clk]
+
+set_property -dict {PACKAGE_PIN V15 IOSTANDARD LVCMOS33} [get_ports test]
+set_property PULLTYPE PULLUP [get_ports test]
---
+++ vivado-pullup-test/top.sv
@@ -0,0 +1,24 @@
+module top(
+    input clk,
+    inout test
+);
+    
+    integer counter;
+    logic test_en;
+    
+    assign test = test_en ? 'b1 : 'bz;
+    
+    always_ff @(posedge clk) begin
+        if (counter >= 'h100000) begin
+            counter <= 'b0;
+        end
+        else begin
+            counter <= counter + 'b1;
+        end
+    end
+    
+    always_comb begin
+        test_en = (counter <= 'h80000);      
+    end
+
+endmodule
